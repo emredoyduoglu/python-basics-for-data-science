b054bc672a932ae5332c351ea58cb19d774f4e9d
emredoyduoglu <93899430+emredoyduoglu@users.noreply.github.com>
2021-11-08 11:58:56 +0300

    Create types.py

---
+++ module01-python-basics/types.py
@@ -0,0 +1,48 @@
+print("Types")
+a = 10
+b = 10.6
+c = "Hello Python 101"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print("Typecasting")
+e = float(a)
+f = int(b)
+g = int('88')
+h = int(d)
+i = int(False)
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+print(i)
+print(type(i))
+
+"""
+Types
+<class 'int'>
+<class 'float'>
+<class 'str'>
+<class 'bool'>
+Typecasting
+10.0
+<class 'float'>
+10
+<class 'int'>
+88
+<class 'int'>
+1
+<class 'int'>
+0
+<class 'int'>
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
+"""
