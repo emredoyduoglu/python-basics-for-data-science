b718146e70b53c23af2a29860b4da6ad264c2404
emredoyduoglu <93899430+emredoyduoglu@users.noreply.github.com>
2021-11-08 14:14:12 +0300

    Update string_operations.py

--- module01-python-basics/string_operations.py
+++ module01-python-basics/string_operations.py
@@ -1,1 +1,59 @@
+Name = "Michael Jackson"
+Statement = Name + " is the best"
+print(Name)
+print(Statement)
 
+print(Name[0])
+print(Name[-1])
+
+#Slicing
+print(len(Name))
+print(Name[0:4])
+print(Name[8:12])
+
+#Stride
+print(Name[0:8:2])
+print(Name[::2])
+
+#Escape sequences : \ are meant to proceed escape sequences
+print("Michael Jackson \nis the best")
+print("Michael Jackson \tis the best")
+print("Michael Jackson \\is the best")
+
+#Methods
+#upper() - lower()
+A = "Thriller is the sixth studio album"
+print(A.upper())
+print(A.lower())
+
+#replace()
+print(Statement.replace('Michael','Janet'))
+
+#find()
+print(Name.find('a'))
+print(Name.find('Jack'))
+
+"""
+Michael Jackson
+Michael Jackson is the best
+M
+n
+15
+Mich
+Jack
+Mcal
+McalJcsn
+Michael Jackson 
+is the best
+Michael Jackson 	is the best
+Michael Jackson \is the best
+THRILLER IS THE SIXTH STUDIO ALBUM
+thriller is the sixth studio album
+Janet Jackson is the best
+4
+8
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
+"""
