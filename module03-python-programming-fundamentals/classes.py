80d9219cb7d8b8061f3e6dcedc81d098319e9d45
Emrah Emre Doyduoğlu <93899430+emredoyduoglu@users.noreply.github.com>
2021-11-12 09:54:58 +0300

    Update classes.py

--- module03-python-programming-fundamentals/classes.py
+++ module03-python-programming-fundamentals/classes.py
@@ -24,3 +24,29 @@ RedCircle = Circle(10, 'red')
 RedCircle.drawCircle()
 print(dir(RedCircle))
 
+#Python Inheritance Syntax
+
+class Polygon:
+    def __init__(self, no_of_sides):
+        self.n = no_of_sides
+        self.sides = [0 for i in range(no_of_sides)]
+
+    def inputSides(self):
+        self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
+
+    def dispSides(self):
+        for i in range(self.n):
+            print("Side",i+1,"is",self.sides[i])
+
+#This class has data attributes to store the number of sides n and magnitude of each side as a list called sides.
+
+class Triangle(Polygon):
+    def __init__(self):
+        Polygon.__init__(self,3)
+
+    def findArea(self):
+        a, b, c = self.sides
+        # calculate the semi-perimeter
+        s = (a + b + c) / 2
+        area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+        print('The area of the triangle is %0.2f' %area)
